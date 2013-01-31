4c927a4359e701a970f9665f9dd77a10e4ee160c
Victor Montoya <vhmontoya@gmail.com>
2013-01-31 14:22:45 -0500

    ProyectoFinalAA
    ProyectoFinalArquitecturaA

---
+++ ventasonline/2013-01-28-17_59.sql
@@ -0,0 +1,19 @@
+/*
+SQLyog Enterprise - MySQL GUI v8.05 
+MySQL - 5.0.27-community-nt : Database - ventasonline_db
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`ventasonline_db` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `ventasonline_db`;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
